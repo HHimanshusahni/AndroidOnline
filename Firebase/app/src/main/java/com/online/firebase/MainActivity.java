a2c4788b0e12ddf9b38ce7e7cc0bc4c1d2138c7e
Himanshu Sahni <sahnihimanshu97@gmail.com>
2018-07-13 22:09:35 +0530

    Writing data to Firebase Realtime Database Note taking

--- Firebase/app/src/main/java/com/online/firebase/MainActivity.java
+++ Firebase/app/src/main/java/com/online/firebase/MainActivity.java
@@ -2,50 +2,44 @@ package com.online.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button button;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btnDb);
+        editText = findViewById(R.id.etNote);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String note = editText.getText().toString();
+                // Upload  the note to Firebase
+
+                // getting reference to root database
+                // .setvalue() to upload the value to the root node in console
+                // but this updates the existing value at the node instead of creating new node
+
+//                FirebaseDatabase.getInstance().getReference().setValue(note);
+
+
+                // push creates the new node for every value
+                FirebaseDatabase.getInstance().getReference().push().setValue(note);
+
+
+
+            }
+        });
     }
 }
-/*
----------------------------------------------------------
- How to integrate cloud database to our  app
 
-Databases>> Add Realtime data bases
-
-        locked mode -> pvt database
-                    -> no value can added from app to console
-                    -> only value from console can be added
-                        to the app
-        test mode -> everone will be able to write data to database
-
- select test mode
- can change the  rules later on
-
-
-    Now we can add value in data base from the console
-
-    KEY --VALUE PAIRING OF THE DATABASE
-    --> json format
-    --> not a sql database
-    --> we can only store primitives data types here
-
-    EXPORT JSON
-                downloads database in json format
-
-In IDE
-
-Assitant> RealtimeDatabase> Click play button
-        > Add the Realtime Database to your app
-
-adds dependency update it in the gradle file
-----------------------------------------------------
-
-
-
-
-*/
