7c21a8e85bdb0bb3b97e14455741db1f8df832e6
Himanshu Sahni <sahnihimanshu97@gmail.com>
2018-07-13 20:55:19 +0530

    Writing data to Firebase Realtime database Notes

--- Firebase/app/src/main/java/com/online/firebase/MainActivity.java
+++ Firebase/app/src/main/java/com/online/firebase/MainActivity.java
@@ -11,3 +11,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+/*
+---------------------------------------------------------
+ How to integrate cloud database to our  app
+
+Databases>> Add Realtime data bases
+
+        locked mode -> pvt database
+                    -> no value can added from app to console
+                    -> only value from console can be added
+                        to the app
+        test mode -> everone will be able to write data to database
+
+ select test mode
+ can change the  rules later on
+
+
+    Now we can add value in data base from the console
+
+    KEY --VALUE PAIRING OF THE DATABASE
+    --> json format
+    --> not a sql database
+    --> we can only store primitives data types here
+
+    EXPORT JSON
+                downloads database in json format
+
+In IDE
+
+Assitant> RealtimeDatabase> Click play button
+        > Add the Realtime Database to your app
+
+adds dependency update it in the gradle file
+----------------------------------------------------
+
+
+
+
+*/
